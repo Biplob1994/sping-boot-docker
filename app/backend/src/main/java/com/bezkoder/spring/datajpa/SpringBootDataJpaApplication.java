394f5660e1b49a246686fa4a4354a02f76a9c8a1
SABBiR <team.cloudboost@gmail.com>
2022-10-27 00:08:46 +0600

    Enable SpringBootServletInitializer

--- app/backend/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ app/backend/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -2,9 +2,11 @@ package com.bezkoder.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Import SpringBootServeletInitailizer to build war file. for more : https://www.baeldung.com/spring-boot-war-tomcat-deploy
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDataJpaApplication {
+public class SpringBootDataJpaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
